f8714b3fb43395c1b0d1149bb660a1d47a865824
Askar <osbelkz@gmail.com>
2020-09-23 14:52:03 +0600

    storybook: add todolist api stories

---
+++ src/stories/todolists-api.stories.tsx
@@ -0,0 +1,50 @@
+import React, {useEffect, useState} from 'react'
+import {todolistAPI} from "../api/todolist-api";
+
+export default {
+    title: 'API'
+}
+
+
+
+export const GetTodolists = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        todolistAPI.getTodolists()
+            .then(res => {
+                setState(res.data)
+            })
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
+export const CreateTodolist = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        const title = "CSS"
+        todolistAPI.createTodolist(title)
+            .then(res => {
+                setState(res.data)
+            })
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
+export const DeleteTodolist = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        todolistAPI.deleteTodolist("a6ad2b82-6d9d-4a08-b9ed-bb2003166268")
+        .then(res => setState(res.data))
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
+export const UpdateTodolistTitle = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        todolistAPI.updateTodolistTitle("4b00442c-8d53-4e16-824f-2f86e615fc04", "From api")
+            .then(res => setState(res.data))
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
