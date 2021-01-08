170b1bcebbd4374781ec87dc7705b73143ef4345
Askar <osbelkz@gmail.com>
2021-01-08 13:11:02 +0600

    change api key

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -4,7 +4,7 @@ import {GetTasksResponse, LoginParamsType, ResponseType, TaskType, TodolistType,
 const settings = {
     withCredentials: true,
     headers: {
-        'API-KEY': '1cdd9f77-c60e-4af5-b194-659e4ebd5d41'
+        'API-KEY': '2dece0c4-7aed-430e-aeba-9f10430f969a'
     }
 }
 const instance = axios.create({
